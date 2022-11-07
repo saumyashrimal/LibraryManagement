9852494d094545280b5117e23220305b8a644e39
Saumya Shrimal <saumyashrimal@Saumyas-MacBook-Air.local>
2022-11-08 01:19:43 +0530

    Server Created

---
+++ Server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+//import mongo client
+const mongoClient = require("mongodb").MongoClient;
+
+const dburl = process.env.DATABASE_URL;
+
+mongoClient.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if(err){
+        console.log('error in db connection', err);
+    }
+    else{
+        //cretae database obj
+        let databaseObj = client.db("LibraryManagement");
+
+        // create collection obj
+        let bookCollection = databaseObj.collection('bookscollection');
+        let userCollection = databaseObj.collection('userCollection');
+
+        // when making api calls - we get the the collections in req object
+        app.set('bookCollection', bookCollection);
+        app.set('userCollection', userCollection);
+    }
+
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port,() => console.log(`server is listening to port ${port}.....`))
+
